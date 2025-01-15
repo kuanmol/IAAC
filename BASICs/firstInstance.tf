337b30f2cace1efd2c0a7f1f18a9a8053d14c985
kuanmol <ramuklomna100@gmail.com>
2025-01-15 20:20:32 +0530

    multiple instance

--- BASICs/firstInstance.tf
+++ BASICs/firstInstance.tf
@@ -1,10 +1,9 @@
-provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "us-east-1"
-}
-
 resource "aws_instance" "MyFirstInstance" {
+
+  count = 3  //for multiple instance at once
   ami           = "ami-0c7af5fe939f2677f"
   instance_type = "t2.micro"
+  tags = {
+    Name = "three-instances"
+  }
 }
